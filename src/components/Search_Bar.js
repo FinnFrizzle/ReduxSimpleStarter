1a7fac929566bc6cda2b2a8c20f51e0048618d66
Eugene Sokol <Eugenesokol97@gmail.com>
2017-07-14 10:19:06 +0200

    Rettelse af search

--- src/components/Search_Bar.js
+++ src/components/Search_Bar.js
@@ -10,14 +10,15 @@ class SearchBar extends Component {
  render() {
    return (
      <div className="search-bar">
-       <input
+       <input type='text'
        value={this.state.term}
-       onChange={event => this.onInputChange({term: event.target.value()})} />
+       onChange={event => this.onInputChange(event.target.value)}
+      />
      </div>
     );
   }
 
-  onInputChange (term) {
+  onInputChange(term) {
     this.setState({term})
     this.props.onSearchTermChange(term)
   }
